eeb3283e7548bb203c787379009fbed3e1ec7083
m-ghiasi <mahdiye71gh@gmail.com>
2024-11-20 12:17:16 +0330

    background detail

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ import './public/style.css'
 import javascriptLogo from './javascript.svg'
 import 'keen-slider/keen-slider.min.css'
 import KeenSlider from 'keen-slider'
+import {topfunction} from './public/topfunction.js'
 
+const upBtn = document.getElementById("up-btn")
 
-
+ topfunction(upBtn) 
---
+++ public/topfunction.js
@@ -0,0 +1,9 @@
+export function topfunction(element1){
+
+    element1.addEventListener('click', ()=> {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    })
+    
+}
+
